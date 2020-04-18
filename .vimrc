f6de34566cc39b1db576938be9e502f1320a4d18
techno1731 <techno1731@gmail.com>
2020-04-18 14:27:08 +0200

    Updated files configuration and added sh file to run it all from a single command

--- .vimrc
+++ .vimrc
@@ -145,3 +145,5 @@ let g:slime_target = "tmux"
 let g:slime_python_ipython = 1
 let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
 
+" Autoload NerTree on startup
+autocmd VimEnter * NERDTreeToggle 
--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-tmux new-session -d 'vim'
-tmux split-window -v 'ipython'
-tmux split-window -h
+session='Udacity-Project-2'
+tmux new-session -d -s $session
+tmux rename-window -t 0 'VIM'
+tmux send-keys -t 'VIM' 'vim ' IMDbAnalysis.py C-m
+tmux split-window -v 
+tmux split-window -h 'ipython'
 tmux new-window 'mutt'
 tmux -2 attach-session -d
